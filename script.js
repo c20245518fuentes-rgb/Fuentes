7bfa57f52aa2cf8e7b423b7976089b15d7146eca
c20245518fuentes-rgb <c20245518.fuentes@csav.edu.ph>
2026-09-04 14:39:00 +0800

    Variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log ("Hello, World!");
-console.log ("from script.js file.");
+console.log("Hello world.");
+console.log("From script.js file.");
+
+const myName = "Tharver";
+let age = 60;
+const number = "09936898115";
+const address = "Victorias City Brgy 6-A Zone 3 Pasil";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
