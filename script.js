8f5b9e511a9333e14c4faeebf8559786176890b6
c20245518fuentes-rgb <c20245518.fuentes@csav.edu.ph>
2026-09-04 15:38:09 +0800

    Functions

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ console.log(`Age: ${age}`);
 console.log(`Number: ${number}`);
 console.log(`Address: ${address}`);
 console.log(67);
+
+
+function greet(greeting, names = []) {
+	if (Array.isArray(names)) {
+		names = names.join(", ");
+	}
+	return `${greeting} sa enyu ${names}`;
+}
+console.log(greet("Mayung aga", ["Mark", "Bai", "Soy", "Tol", "Erp"]));
+
