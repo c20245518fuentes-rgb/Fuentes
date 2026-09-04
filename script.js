6326c9d0a4f4789ce6e3c359006a4d58e913c468
c20245518fuentes-rgb <c20245518.fuentes@csav.edu.ph>
2026-09-04 17:08:27 +0800

    style

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ console.log(greet("Mayung aga", ["Mark", "Bai", "Soy", "Tol", "Erp"]));
 
 const heading = document.querySelector("h1");
 heading.textContent = "Your father's name";
+heading.style.color = "purple";
+heading.style.backgroundColor = "yellow";
+heading.style.fontSize = "100px";
 console.log(heading);
 
 const contactHeading = document.querySelector("#contact h2");
